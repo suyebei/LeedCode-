bebecd73aa91fe31646ef002699b0eb99533a0c9
suyebei <1761480432@qq.com>
2020-02-26 22:48:01 +0800

    leetcode142

---
+++ leetcode142.c
@@ -0,0 +1,35 @@
+/*************************************************************************
+	> File Name: leetcode142.c
+	> Author: suyebei
+	> Mail: suyebei@126.com
+	> Created Time: 三  2/26 22:45:27 2020
+ ************************************************************************/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode *detectCycle(struct ListNode *head) {
+    struct ListNode *p = head, *q = head;
+    if (p == NULL) return NULL;
+    do {
+        p = p->next;
+        q = q->next;
+        if (q == NULL || q->next == NULL) {
+            return NULL;
+        }
+        q = q->next;
+    } while (p != q);
+    int cnt = 0;
+    do {
+        cnt++;
+        p = p->next;
+    } while (p != q);
+    p = head, q = head;
+    while (cnt--) q = q->next;
+    while (p != q) p = p->next, q = q->next;
+    return p;  
+}
