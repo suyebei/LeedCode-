b90ce617508ecc750db209e63c1fd6ab52c40656
suyebei <1761480432@qq.com>
2020-02-29 18:49:31 +0800

    翻转二叉树

---
+++ leetcode226.cpp
@@ -0,0 +1,26 @@
+/*************************************************************************
+	> File Name: leetcode226.cpp
+	> Author: suyebei
+	> Mail: suyebei@126.com
+	> Created Time: 六  2/29 18:47:02 2020
+ ************************************************************************/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+
+
+struct TreeNode* invertTree(struct TreeNode* root){
+    if (root == NULL) return NULL;
+    struct TreeNode *temp = root->left;
+    root->left = root->right;
+    root->right = temp;
+    invertTree(root->left);
+    invertTree(root->right);
+    return root;
+}
